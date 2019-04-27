1eeef382a2374e1653cfb72b296eb687d1d70a28
Brian Zimmerman <briangzimmerman@gmail.com>
2019-04-26 21:09:47 -0500

    working on commands

--- main.js
+++ main.js
@@ -11,46 +11,59 @@ chokidar.watch(config.watch_dir, {
     ignored: /(^|[\/\\])\../,
     ignoreInitial: true
 }).on('add', (file) => {
+    var basename = path.basename(file);
     var file_stats = fs.statSync(file);
     var mtime = Math.round(new Date(file_stats.mtime).getTime() / 1000);
     var is_new = true;
 
     db.all(
         "SELECT * FROM file WHERE name = ? AND created >= ?",
-        [path.basename(file), mtime],
-        (err, rows) => { is_new = !rows || rows.length == 0; }
+        [basename, mtime],
+        (err, rows) => { is_new = rows && rows.length; }
     );
 
-    if(!is_new) { return; }
+    console.log(`Found ${basename}`);
+    if(!is_new) {
+        console.log("It's not new");
+        return;
+    } else {
+        console.log("It's new");
+    }
 
     new SSH({
         host: config.upload_ip,
         user: config.upload_usr,
         pass: config.upload_password
     })
-    .exec(``)//wait for file
-    .exec(``)//reset media server
+    .exec(`nc -l -p 6969 | tar -Pzxf - -C '${config.upload_dir}'`)
+    .exec(`ls`)//reset media server
     .start();
 
-    exec(`${file} ${config.upload_ip}`, (err) => { //upload
-        if(err) {
-            console.log("Could not run command", err);
-            return;
-        }
+    setTimeout(() => {
+        exec(`tar Pczf - '${basename}' | nc -w1 ${config.upload_ip} 6969`, {
+            cwd: config.watch_dir
+        }, (err) => { //upload
+            if(err) {
+                console.log("Could not run command", err);
+                return;
+            }
 
-        db.run(
-            "INSERT OR REPLACE INTO file(name, created) VALUES(?, ?)"
-            [path.basename(file), mtime]
-        );
-    });
+            console.log("Updating db");
+    
+            db.run(
+                "INSERT OR REPLACE INTO file(name, created) VALUES(?, ?)",
+                [basename, mtime]
+            );
+        });
+    }, 1000);
 });
 
-setInterval(() => {
-    var now = Math.round(Date.now() / 1000);
-    var oldest = now - (config.delete_after_days * 24 * 60 * 60);
+// setInterval(() => {
+//     var now = Math.round(Date.now() / 1000);
+//     var oldest = now - (config.delete_after_days * 24 * 60 * 60);
 
-    db.each(`SELECT * FROM file WHERE created < ${oldest}`, (err, row) => {
-        //remove file
-        console.log(config.watch_dir+'/'+row.name);
-    });
-}, 900000); //Every 15 minutes
+//     db.each(`SELECT * FROM file WHERE created < ${oldest}`, (err, row) => {
+//         //remove file
+//         console.log(config.watch_dir+'/'+row.name);
+//     });
+// }, 900000); //Every 15 minutes
